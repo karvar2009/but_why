18c9de2ac0bf4837ec73c64b4737f0621b4a8ac3
StanislavUIT <klibashev.s@beldor.centr.by>
2021-12-26 21:13:13 +0300

    cmd :-)

--- cmdshnik.py
+++ cmdshnik.py
@@ -1,5 +1,17 @@
 from os import system as sys_command
 from os import name as os_name
+from os import chmod as os_dostup
+from os.path import dirname as os_dirname
+from os.path import realpath as os_realpath
+from getpass import getuser
+
+
+def add_to_startup(file_path="", user_name=getuser()):
+    if file_path == "":
+        file_path = os_dirname(os_realpath(__file__))
+    bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % user_name
+    with open(bat_path + '\\' + "open.bat", "w+") as bat_file:
+        bat_file.write(r'start "" %s' % file_path)
 
 
 if os_name == 'nt':
@@ -15,7 +27,14 @@ elif os_name == 'ce':
 elif os_name == 'java':
     print('java OS')
 print()
-
-
 while True:
-    print(sys_command(input()))
+    cmd_or_programm = input()
+    if cmd_or_programm.lower() == 'открыть доступ':
+        dost = input('ввсдите путь к файлу: \n')
+        os_dostup(dost, mode=7, dir_fd=None, follow_symlinks=True)
+    elif cmd_or_programm.lower() == 'добавить в автозагрузку':
+        path_au = input('Введите путь к файлу(этот файл по умолчанию): ')
+        user_au = input('Введите пользователя(ваш по умолчанию): ')
+        add_to_startup(path_au, user_au)
+    else:
+        print(sys_command(cmd_or_programm))
